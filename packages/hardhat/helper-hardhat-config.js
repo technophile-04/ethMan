dff705e9f419ff527eff5d4500f33c62511c5c15
Shiv Bhonde <shivbhonde04@gmail.com>
2022-08-13 22:21:19 +0530

    add mockv3Aggregator and deploy scripts

---
+++ packages/hardhat/helper-hardhat-config.js
@@ -0,0 +1,17 @@
+const networkConfig = {
+  rinkeby: {
+    ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+    blockConfirmations: 6,
+  },
+  mumbai: {
+    ethUsdPriceFeed: "0x0715A7794a1dc8e42615F059dD6e406A6594651A",
+    blockConfirmations: 6,
+  },
+};
+
+const developmentChains = ["hardhat", "localhost"];
+
+const DECIMALS = 8;
+const INITAL_ANSWERE = 200000000000;
+
+export { networkConfig, developmentChains, DECIMALS, INITAL_ANSWERE };
